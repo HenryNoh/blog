2f2f1fbf618fc582baa17c8ac885c6ba1772af9b
xodlf8044 <xodlf8044@gmail.com>
2020-05-17 22:10:51 +0900

    1.0.1

--- server/server.js
+++ server/server.js
@@ -1,24 +1,36 @@
 const express = require('express');
 const app = express();
-const PORT = process.env.PORT || 4000;
-const db = require('./config/db');
+const sequelize = require('./models').sequelize;
+sequelize.sync();
+app.use(express.json());
+const bodyParser = require('body-parser')
 
-app.get('/api/host', (req, res) => {
-    res.send({ host : 'sejun' });
-})
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
-app.get('/api/test', (req, res) => {
-    db.query("select * from test", (err, data) => {
-        if(!err) {
-            res.send(data);
 
-        } else {
-            console.log(err);
-            res.send(err);
-        }
+const {
+    Teacher,
+    Sequelize: { Op }
+  } = require('./models');
+sequelize.query('SET NAMES utf8;');
+
+app.post('/add/data', (req, res) => {
+    console.log(req.body)
+
+    Teacher.create({
+        name : req.body.data
+    })
+    .then( result => {
+        res.send(result)
+    })
+    .catch( err => {
+        console.log(err)
+        throw err;
     })
 })
 
+const PORT = process.env.PORT || 4000;
 app.listen(PORT, () => {
     console.log(`Server On : http://localhost:${PORT}/`);
 })
--- src/App.js
+++ src/App.js
@@ -6,23 +6,41 @@ class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      host : '',
+      name : '',
     }
   }
 
-  componentDidMount() {
-    this._dbTest();
+  _addData = async(e) => {
+    const { name } = this.state;
+    e.preventDefault();
+    
+    const res = await axios('/add/data', {
+      method : 'POST',
+      data : { 'data' : name },
+      headers: new Headers()
+    })
+
+    if(res.data) {
+      alert('데이터를 추가했습니다.');
+      return window.location.reload();
+    }
   }
-  
-  _dbTest = async() => {
-    const res = await axios.get('/api/test');
-    console.log(res.data)
+
+  _nameUpdate(e) {
+    this.setState({ name : e.target.value })
   }
 
   render() {
     return(
       <div className='App'>
-        <h3> Welcome to <u> {this.state.host} </u> Blog! </h3>
+        <h3> Welcome to <u> sejun </u> Blog! </h3>
+        <h5> https://sejun3278.blog.me/ </h5>
+
+        <br />
+        <form method='POST' onSubmit={this._addData}>
+          <input type='text' maxLength='10' onChange={(e) => this._nameUpdate(e)}/>
+          <input type='submit' value='Add' />
+        </form>
       </div>
     )
   }
